8e84007e74653aacdedabbf80c73f11f33f2b604
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-13 13:00:59 +0300

    clear finc

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+class Calculator {
+    constructor(previousOperandTextElemnet, currentOperandTextElemnet) {
+        this.previousOperandTextElemnet = previousOperandTextElemnet;
+        this.currentOperandTextElemnet = currentOperandTextElemnet;
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = '';
+        thies.previousOperand = '';
+        thies.operation = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number')
 const operationButtons = document.querySelectorAll('[data-operation')
 const equalsButton = document.querySelector('[data-equals]')
